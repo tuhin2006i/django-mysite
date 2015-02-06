7a5846440d95e39382f7f87141b966fd609ea4bc
Md. Shahadat Hossain Chowdhury <tuhin2006i@gmail.com>
2015-02-06 16:29:20 +0100

    changed file locations

--- polls/admin.py
+++ polls/admin.py
@@ -4,8 +4,8 @@ from polls.models import Question
 class QuestionAdmin(admin.ModelAdmin):
     #fields = ['pub_date', 'question_text']
     fieldsets = [
-	(None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+                 (None,               {'fields': ['question_text']}),
+                 ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
 
 admin.site.register(Question, QuestionAdmin)
