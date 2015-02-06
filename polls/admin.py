d6932eb9806b75d6324465b5b4ecc7ff0f2d7e21
Md. Shahadat Hossain Chowdhury <tuhin2006i@gmail.com>
2015-02-06 14:57:41 +0100

    First commit

---
+++ polls/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from polls.models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+    fieldsets = [
+	(None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
